bcb8e1dc119de797f6d0a13a1afe1e01bbec2e75
LeahJKH <Leahb06.kodehode@gmail.com>
2024-08-07 18:48:31 +0200

    small changes

--- Person.cs
+++ Person.cs
@@ -11,9 +11,20 @@ public class Person {
     public void Getperson(string Name, int Age) {
         if (Age >= 18 ) {
         Console.WriteLine($"welcome, {Name}! you are {Age}!");
+        SceneTwo(Name, Age)
         }
         else {
             Console.WriteLine($"{Name}! you are {Age}! Get off the site!!!");
+        }
+    }
+    public void SceneTwo(Name, Age) {
+        Console.WriteLine("so you wake up in a damp cellar")
+        Console.WriteLine($"{Name}- this is why i dont drink often")
+        Console.WriteLine("you see a door and a bed something is beside you")
+        action = Console.ReadLine()
+        if(action.ToLower.Contains("bed")) 
+        {
+            
         }
     }
     

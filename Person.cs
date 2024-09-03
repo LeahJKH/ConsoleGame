28134a9847f94e2869a962c4abff021b5ebd5c83
LeahJKH <leahjb06.kodehode@gmail.com>
2024-09-03 10:16:29 +0200

    fixed bugs

--- Person.cs
+++ Person.cs
@@ -1,31 +1,33 @@
-using System.Dynamic;
-
-public class Person {
-
-    public string? Name {get; set;}
-    public int? Age {get; set;}
-    public Person (string? name, int? age){
-        Name = name;
-        Age = age;
-    }
-    public void Getperson(string Name, int Age) {
-        if (Age >= 18 ) {
-        Console.WriteLine($"welcome, {Name}! you are {Age}!");
-        SceneTwo(Name, Age)
-        }
-        else {
-            Console.WriteLine($"{Name}! you are {Age}! Get off the site!!!");
-        }
-    }
-    public void SceneTwo(Name, Age) {
-        Console.WriteLine("so you wake up in a damp cellar")
-        Console.WriteLine($"{Name}- this is why i dont drink often")
-        Console.WriteLine("you see a door and a bed something is beside you")
-        action = Console.ReadLine()
-        if(action.ToLower.Contains("bed")) 
+public class Person
+    {
+        public string? Name { get; set; }
+        public int? Age { get; set; }
+        public Person(string? name, int? age)
         {
-            
+            Name = name;
+            Age = age;
+        }
+        public void Getperson(string Name, int Age)
+        {
+            if (Age >= 18)
+            {
+                Console.WriteLine($"Welcome, {Name}! You are {Age}!");
+                SceneTwo(Name, Age);
+            }
+            else
+            {
+                Console.WriteLine($"{Name}! You are {Age}! Get off the site!!!");
+            }
+        }
+        public void SceneTwo(string Name, int Age)
+        {
+            Console.WriteLine("So you wake up in a damp cellar");
+            Console.WriteLine($"{Name}- this is why I don't drink often");
+            Console.WriteLine("You see a door and a bed. Something is beside you. What do you do?");
+            string action = Console.ReadLine(); 
+            if (action.ToLower().Contains("bed")) 
+            {
+                Console.WriteLine("You check the bed.");
+            }
         }
     }
-    
-}
--- Program.cs
+++ Program.cs
@@ -1,52 +1,50 @@
-﻿namespace personConstruct;
-    class Program 
+﻿namespace personConstruct
+{
+    class Program
     {
-    static void Main(string[] args) {
-
-        Person newPerson = new Person(null, null);
-        Console.WriteLine("please enter name");
-        string namerequest = Console.ReadLine();
-        string name = namerequest;
-        Console.WriteLine("please enter age");
-        int agerequest = Convert.ToInt32(Console.ReadLine());
-        int age = agerequest;
-        newPerson.Name = name;
-        newPerson.Age = age;
-
-        
-
-        newPerson.Getperson(name, age);
-        Console.WriteLine($"lets get started, {name}");
-        Console.WriteLine($"you where a {age} old.... okay whats your gender actually");
-        string gender = Console.ReadLine();
-        Console.WriteLine($"Great, now that i know you are {gender} we can continue");
-        Console.WriteLine("");
-        Console.WriteLine("");
-        Console.WriteLine("first of all choose a class");
-        Console.WriteLine("1: mage");
-        Console.WriteLine("2: barbarian");
-        Console.WriteLine("3: swordsman");
-         Console.WriteLine("WRITE NUMBER OF THE CLASS YOU WANT:");
-         
-        int yourClass = Convert.ToInt32(Console.ReadLine());
-        if (yourClass <= 3 || yourClass >= 1)
+        static void Main(string[] args)
         {
-                if(yourClass == 1) {
-            Console.WriteLine("you choose mage");
+            Person newPerson = new Person(null, null);
+            Console.WriteLine("please enter name");
+            string namerequest = Console.ReadLine()!;
+            string name = namerequest;
+            Console.WriteLine("please enter age");
+            int agerequest = Convert.ToInt32(Console.ReadLine());
+            int age = agerequest;
+            newPerson.Name = name;
+            newPerson.Age = age;
+
+            newPerson.Getperson(name, age);
+            Console.WriteLine($"lets get started, {name}");
+            Console.WriteLine($"you were {age} years old.... okay, what's your gender actually?");
+            string gender = Console.ReadLine();
+            Console.WriteLine($"Great, now that I know you are {gender}, we can continue");
+            Console.WriteLine("");
+            Console.WriteLine("first of all, choose a class");
+            Console.WriteLine("1: mage");
+            Console.WriteLine("2: barbarian");
+            Console.WriteLine("3: swordsman");
+            Console.WriteLine("WRITE NUMBER OF THE CLASS YOU WANT:");
+
+            int yourClass = Convert.ToInt32(Console.ReadLine());
+            if (yourClass >= 1 && yourClass <= 3) // Corrected condition
+            {
+                if (yourClass == 1)
+                {
+                    Console.WriteLine("you chose mage");
+                }
+                else if (yourClass == 2)
+                {
+                    Console.WriteLine("you chose barbarian");
+                }
+                else if (yourClass == 3)
+                {
+                    Console.WriteLine("you chose swordsman");
+                }
             }
-            if(yourClass == 2) {
-            Console.WriteLine("you choose barbarian");
+            else
+            {
+                Console.WriteLine("don't be an idiot");
             }
-            if(yourClass == 3) {
-            Console.WriteLine("you choose swordsman");
-            }
-        } else {
-            Console.WriteLine("dont be an idiot");
         }
-      
-        
-    }
-    }
-
-
-
+    }}
